1bfbb958a5def0ffb0653aaf1100236e6ca76397
Brian Clark <couchninja1@gmail.com>
2025-10-01 15:12:06 -0500

    Updates per feedback

--- tests/careers/apply.careers.negative.spec.js
+++ tests/careers/apply.careers.negative.spec.js
@@ -2,22 +2,17 @@ import { test, expect } from "@playwright/test";
 import { HomePage } from "../../page_objects/HomePage";
 import { CareersPage } from "../../page_objects/CareersPage";
 
-test.beforeEach("homepage logo is visible", async ({ page }) => {
-  const homePage = new HomePage(page);
-  await page.goto("/");
-  //commented code to force a commit
-
-  await expect(homePage.mainLogo).toBeVisible();
-});
-
-test("navigate to the application page and submit without filling in any fields", async ({ page }) => {
+test("navigate to the application page and submit without filling in any fields", async ({
+  page,
+}) => {
   const homePage = new HomePage(page);
   const careersPage = new CareersPage(page);
   const list = careersPage.careersListContainer;
 
+  await page.goto("/");
+  await expect(homePage.mainLogo).toBeVisible();
   await homePage.careersMenuLink.click();
 
-  await expect(list).toBeVisible();
   await expect(list.getByRole("link")).not.toHaveCount(0);
 
   await list.getByRole("link").first().click();
